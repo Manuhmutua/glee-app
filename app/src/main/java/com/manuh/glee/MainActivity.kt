7cc8bbc91b58342f13ba2d8b6e69399f26b5e4f6
Manuhmutua <manuh.mutuah@gmail.com>
2020-04-02 11:50:32 +0300

    first commit

---
+++ app/src/main/java/com/manuh/glee/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.manuh.glee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manuh.glee.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/manuh/glee/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+
+package com.manuh.glee.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
